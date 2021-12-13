2440770aaa7dc1fed672b85f50df808f210ee858
Anjani Bonam <95603359+anjanibonam@users.noreply.github.com>
2021-12-13 10:17:20 +0530

    Add files via upload

---
+++ ANJANI switch 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	scanf("%d",&a);
+	switch (a)
+	{
+	case 1:printf("odd");break;
+	case 2:printf("even");break;
+	case 3:printf("odd");break;
+	case 4:printf("even");break;
+	case 5:printf("odd");break;
+	case 6:printf("even");break;
+	case 7:printf("odd");break;
+	case 8:printf("even");break;
+	case 9:printf("odd");break;
+	default:printf("not valid");
+	}
+}
