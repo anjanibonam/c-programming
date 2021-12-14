564d1166f94b29077af0ea3d5c9f25f6b4c3aaa9
Anjani Bonam <95603359+anjanibonam@users.noreply.github.com>
2021-12-14 16:49:32 +0530

    Add files via upload

---
+++ max of 3 nos switch.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+  scanf("%d%d%d",&a,&b,&c);
+  switch(a>b&& a>c)
+  case 1:printf("\na is max");
+  switch(b>a && b>c)
+  case 1:printf("\nb is max");
+  switch(c>a && c>b)
+  case 1:printf("\nc is max");
+}
